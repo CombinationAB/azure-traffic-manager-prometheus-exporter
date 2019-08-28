5267894cefba51c6bffc28fbe0371b443803330f
Rickard Lyrenius <rly@combination.se>
2019-08-28 09:44:52 +0200

    Bumped version

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IMAGE_NAME=combination/azure-traffic-manager-prometheus-exporter
-IMAGE_VERSION=0.0.2
+IMAGE_VERSION=0.0.3
 
 docker build -t $IMAGE_NAME -t $IMAGE_NAME:$IMAGE_VERSION $(dirname $0)
 
